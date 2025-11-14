b73aa2e17b6b5c36d0b12e40b8c58d303dabb77c
Much More <mcaputo@power2cloud.com>
2025-11-14 12:18:41 +0100

    modifiche deploy

--- prisma/migrations/20240530213853_create_session_table/migration.sql
+++ prisma/migrations/20240530213853_create_session_table/migration.sql
@@ -1,18 +1,38 @@
 -- CreateTable
+-- CREATE TABLE "Session" (
+--     "id" TEXT NOT NULL PRIMARY KEY,
+--     "shop" TEXT NOT NULL,
+--     "state" TEXT NOT NULL,
+--     "isOnline" BOOLEAN NOT NULL DEFAULT false,
+--     "scope" TEXT,
+--     "expires" DATETIME,
+--     "accessToken" TEXT NOT NULL,
+--     "userId" BIGINT,
+--     "firstName" TEXT,
+--     "lastName" TEXT,
+--     "email" TEXT,
+--     "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+--     "locale" TEXT,
+--     "collaborator" BOOLEAN DEFAULT false,
+--     "emailVerified" BOOLEAN DEFAULT false
+-- );
+
 CREATE TABLE "Session" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "shop" TEXT NOT NULL,
-    "state" TEXT NOT NULL,
-    "isOnline" BOOLEAN NOT NULL DEFAULT false,
-    "scope" TEXT,
-    "expires" DATETIME,
-    "accessToken" TEXT NOT NULL,
-    "userId" BIGINT,
-    "firstName" TEXT,
-    "lastName" TEXT,
-    "email" TEXT,
-    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
-    "locale" TEXT,
-    "collaborator" BOOLEAN DEFAULT false,
-    "emailVerified" BOOLEAN DEFAULT false
-);
+     "id" TEXT NOT NULL,
+     "shop" TEXT NOT NULL,
+     "state" TEXT NOT NULL,
+     "isOnline" BOOLEAN NOT NULL DEFAULT false,
+     "scope" TEXT,
+     "expires" TIMESTAMP WITHOUT TIME ZONE,
+     "accessToken" TEXT NOT NULL,
+     "userId" BIGINT,
+     "firstName" TEXT,
+     "lastName" TEXT,
+     "email" TEXT,
+     "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+     "locale" TEXT,
+     "collaborator" BOOLEAN DEFAULT false,
+     "emailVerified" BOOLEAN DEFAULT false,
+
+     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+ );
